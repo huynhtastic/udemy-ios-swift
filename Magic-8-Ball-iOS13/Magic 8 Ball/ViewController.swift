3b26ea131371096957605067fb9e0e5e4041f91c
Richard Huynh <huynhtastic@gmail.com>
2022-04-03 15:52:53 -0500

    add basic features

--- Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
+++ Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
@@ -11,7 +11,11 @@ import UIKit
 class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
+    @IBOutlet weak var imageview: UIImageView!
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        imageview.image = #imageLiteral(resourceName: "ball3")
+    }
+    
 
 
 }
