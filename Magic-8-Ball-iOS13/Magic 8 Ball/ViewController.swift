16af302319adfe99bd04a56a9c21a6ae52259818
Richard Huynh <huynhtastic@gmail.com>
2022-04-04 20:54:55 -0500

    show random element for magic 8 ball

--- Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
+++ Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
     @IBOutlet weak var imageview: UIImageView!
     @IBAction func askButtonPressed(_ sender: UIButton) {
-        imageview.image = #imageLiteral(resourceName: "ball3")
+        imageview.image = ballArray.randomElement()
     }
     
 
