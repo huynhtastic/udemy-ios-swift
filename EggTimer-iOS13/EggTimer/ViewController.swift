a8baacf5991145604cd57f54cc63204651cf03d0
Richard Huynh <huynhtastic@gmail.com>
2022-04-11 22:38:36 -0500

    show countdown timer on hardness btn press

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var timerLabel: UILabel!
+    let eggTimes: [String: Int] = ["Soft": 3, "Medium": 4, "Hard": 5]
+    
+    var timer = Timer()
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        if let hardness = sender.currentTitle {
+            timer.invalidate()
+            
+            var time = eggTimes[hardness]!
+            
+            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+                if (time == -1) { timer.invalidate()
+                    self.timerLabel.text = "Done!"
+                } else {
+                    self.timerLabel.text = "\(time) seconds"
+                    time -= 1
+                }
+                
+            }
+
+        }
+    }
+    
 
 
 }
