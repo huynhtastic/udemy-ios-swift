bf94e39a107eb8d545130a64c97cec6194cfc338
Richard Huynh <huynhtastic@gmail.com>
2022-04-13 22:39:13 -0500

    change text for label and choices

--- Destini-LayoutPractice-iOS13/Destini-iOS13/Controller/ViewController.swift
+++ Destini-LayoutPractice-iOS13/Destini-iOS13/Controller/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choiceOneBtn: UIButton!
+    @IBOutlet weak var choiceTwoBtn: UIButton!
+    
+    let story0 = "You see a fork in the road."
+    let choice1 = "Take a left."
+    let choice2 = "Take a right."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        storyLabel.text = story0
+        choiceOneBtn.setTitle(choice1, for: .normal)
+        choiceTwoBtn.setTitle(choice2, for: .normal)
     }
 
 
