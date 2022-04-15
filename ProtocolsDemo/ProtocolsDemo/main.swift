1918f248bc4b88d9936851af106abc7175747494
Richard Huynh <huynhtastic@gmail.com>
2022-04-15 17:11:04 -0500

    finish protocols demo

--- ProtocolsDemo/ProtocolsDemo/main.swift
+++ ProtocolsDemo/ProtocolsDemo/main.swift
@@ -1,11 +1,46 @@
-//
-//  main.swift
-//  ProtocolsDemo
-//
-//  Created by Richard Huynh on 4/15/22.
-//
+protocol CanFly {
+    func fly()
+}
 
-import Foundation
+class Bird {
+    var isFemale = true
+    
+    func layEgg() {
+        if isFemale {
+            print("Make new bird")
+        }
+    }
+}
 
-print("Hello, World!")
+class Eagle: Bird, CanFly {
+    func fly() {
+        print("The eagle flies")
+    }
+    
+    func soar() {
+        print("Glide through air current")
+    }
+}
 
+class Penguin: Bird {
+    func swim() {
+        print("Swims through water")
+    }
+}
+
+struct FlyingMuseum {
+    func flyingDemo(flyingObject: CanFly) {
+        flyingObject.fly()
+    }
+}
+
+struct Airplane: CanFly {
+    func fly() {
+        print("The plane uses its engine to fly")
+    }
+}
+
+let myEagle = Eagle()
+
+let museum = FlyingMuseum()
+museum.flyingDemo(flyingObject: myEagle)
