2691785ddb3301e1a84af8abd3abea9cdbf571d9
Richard Huynh <huynhtastic@gmail.com>
2022-04-14 22:06:35 -0500

    sliders show correct values

--- BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightValueLabel: UILabel!
+    @IBOutlet weak var weightValueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let formattedValue = String(format: "%.2f", sender.value)
+        heightValueLabel.text = "\(formattedValue)m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let formattedValue = Int(sender.value)
+        weightValueLabel.text = "\(formattedValue)Kg"
+    }
 }
 
