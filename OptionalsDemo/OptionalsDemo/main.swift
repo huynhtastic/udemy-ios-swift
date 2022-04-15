b9b3e7d47fb06d72408e0c0f26e6b6f019432163
Richard Huynh <huynhtastic@gmail.com>
2022-04-15 13:22:17 -0500

    finish optionalsdemo

--- OptionalsDemo/OptionalsDemo/main.swift
+++ OptionalsDemo/OptionalsDemo/main.swift
@@ -1,11 +1,40 @@
-//
-//  main.swift
-//  OptionalsDemo
-//
-//  Created by Richard Huynh on 4/15/22.
-//
+// Force un-wrap
+let myOptional: String?
+myOptional = "Richard"
 
-import Foundation
+let text: String = myOptional!
 
-print("Hello, World!")
+// Optional check
+if myOptional != nil {
+    let text: String = myOptional!
+    let text2: String = myOptional!
+} else {
+    print("myOptional was nil")
+}
 
+// Optional Binding
+if let safeOptional = myOptional {
+    let text: String = safeOptional
+    print(text)
+} else {
+    print("myOptional was nil")
+}
+
+// Nil coalescing operator
+var nilOptional: String? = nil
+let notNilText: String = nilOptional ?? "Default Value"
+print(notNilText)
+
+// Optional Structs & Optional Chaining
+struct OptionalStruct {
+    var prop = 123
+    func method() {
+        print("I am struct method")
+    }
+}
+
+let optionalStruct: OptionalStruct?
+optionalStruct = OptionalStruct()
+
+print(optionalStruct?.prop)
+optionalStruct?.method()
