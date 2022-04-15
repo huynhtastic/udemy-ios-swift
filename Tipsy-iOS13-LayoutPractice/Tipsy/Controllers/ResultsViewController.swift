362ba8553cc3610a2001ed0ae2ac476d5020ec79
Richard Huynh <huynhtastic@gmail.com>
2022-04-15 15:37:49 -0500

    link views to controllers

---
+++ Tipsy-iOS13-LayoutPractice/Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Richard Huynh on 4/15/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
