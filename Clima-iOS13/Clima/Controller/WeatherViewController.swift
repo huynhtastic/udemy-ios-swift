03fa7d7975759bfde3a4dfcc2e494bf46a29a474
Richard Huynh <huynhtastic@gmail.com>
2022-04-19 20:09:47 -0500

    change external parameter names

--- Clima-iOS13/Clima/Controller/WeatherViewController.swift
+++ Clima-iOS13/Clima/Controller/WeatherViewController.swift
@@ -50,9 +50,12 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
         textField.text = ""
     }
     
-    func didUpdateWeather(weather: WeatherModel) {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
         print(weather.temperature)
     }
     
+    func didFailWithError(error: Error) {
+        print(error)
+    }
 }
 
