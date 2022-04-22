dcd98e8440eab5114bd36b4509bb1d5ad13ce14f
Richard Huynh <huynhtastic@gmail.com>
2022-04-21 19:16:02 -0500

    make network request for crypto exchange rate

--- ByteCoin-iOS13/ByteCoin/Model/CoinManager.swift
+++ ByteCoin-iOS13/ByteCoin/Model/CoinManager.swift
@@ -11,11 +11,26 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     func getCoinPrice(for currency: String) {
-        
+        let url = "\(baseURL)/\(currency)?apikey=\(APIKEY)"
+        performRequest(with: url)
+    }
+    
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if (error != nil) {
+                    print(error)
+                    return
+                }
+                if let safeData = data {
+                    print(safeData)
+                }
+            }
+        }
     }
 }
