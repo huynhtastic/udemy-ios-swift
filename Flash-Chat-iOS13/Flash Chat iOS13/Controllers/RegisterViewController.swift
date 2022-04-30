44ec143a72683062687a294e0934b53608d666bd
Richard Huynh <huynhtastic@gmail.com>
2022-04-30 16:35:56 -0500

    add error label to register screen

--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -13,13 +13,15 @@ class RegisterViewController: UIViewController {
     
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        errorLabel.text = ""
         
         if let email = emailTextfield.text, let password = passwordTextfield.text {
             Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
                 if let e = error {
-                    print(e.localizedDescription)
+                    self.errorLabel.text = e.localizedDescription
                 } else {
                     self.performSegue(withIdentifier: "RegisterToChat", sender: self)
                 }
