4bc1526c9e4c28529a3f359c27839a1e1e5f6713
Richard Huynh <huynhtastic@gmail.com>
2022-05-05 18:48:25 -0500

    add dicee swiftui version

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Richard Huynh on 5/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
