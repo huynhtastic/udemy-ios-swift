47ae8ef790daee22ed43fc1f4b8265450ba600e3
Richard Huynh <huynhtastic@gmail.com>
2022-05-05 19:14:12 -0500

    create h4x0r news app

---
+++ H4X0R News/H4X0R News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Richard Huynh on 5/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
