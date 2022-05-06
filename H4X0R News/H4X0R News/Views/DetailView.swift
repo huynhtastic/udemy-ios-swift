a1ab8143b53b7a7119729eb069a6df0eaf6982e7
Richard Huynh <huynhtastic@gmail.com>
2022-05-05 20:42:20 -0500

    finish h4x04 news app

---
+++ H4X0R News/H4X0R News/Views/DetailView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailView.swift
+//  H4X0R News
+//
+//  Created by Richard Huynh on 5/5/22.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://google.com")
+    }
+}
+
